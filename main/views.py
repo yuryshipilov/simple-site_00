68f5921375e1fc1bf981a76df698e5cfe09335ce
Yury <yurshipilov@gmail.com>
2020-10-26 23:56:35 +0400

    Created new templates, making form, input, using bootstrap

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http    import HttpResponse
+from .models import Task
+
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title' : 'Главная страница сайта', 'tasks': tasks})
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    return render(request, 'main/create.html')
