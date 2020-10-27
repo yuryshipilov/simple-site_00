04a0a2a6db4f49c844bd40bb30237869cd0f540d
Yury <yurshipilov@gmail.com>
2020-10-27 22:25:55 +0400

    Ending project, added and configured form

--- main/views.py
+++ main/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
     tasks = Task.objects.order_by('-id')
-    return render(request, 'main/index.html', {'title' : 'Главная страница сайта', 'tasks': tasks})
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
 
 def about(request):
     return render(request, 'main/about.html')
 
+
 def create(request):
-    return render(request, 'main/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма не верна'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
